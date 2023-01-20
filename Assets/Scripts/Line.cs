0c5394281e2a7495e143c27fac0602234a5978ea
WakanaIdeta <k021g1162@g.neec.ac.jp>
2023-01-20 16:13:15 +0900

    プレイヤーから上方向に線が出るようになった

--- Assets/Scripts/Line.cs
+++ Assets/Scripts/Line.cs
@@ -10,56 +10,114 @@ public class Line : MonoBehaviour
 
     private PlayerMove player;
 
+    private GameObject box;
+
     public float speed;
 
     public bool isHit;
 
-    public List<Vector2> linePoints; 
+    public bool isExtend;
+
+    public List<Vector3> linePoints; 
 
     private Vector3 pos1;
     private Vector3 pos2;
     private Vector3 pos3;
 
-    private Vector3[] positions;
     // Start is called before the first frame update
     void Start()
     {
+        linePoints = new List<Vector3>();
         line = GetComponent<LineRenderer>();
 
         GameObject playerObj = GameObject.Find("Player");
         player = playerObj.GetComponent<PlayerMove>();
 
         pos1 = player.transform.position;
-        pos1.y += player.transform.localScale.y / 2;
+        //pos1.y += player.transform.localScale.y / 2;
 
         pos2 = player.transform.position;
-        pos2.y += player.transform.localScale.y / 2;
+        //pos2.y += player.transform.localScale.y / 2;
 
+        linePoints.Add(pos1);
+        linePoints.Add(pos1);
         isHit = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        pos1 = player.transform.position;
 
-        if (isHit == false)
+        if(isHit == false && isExtend == false)
         {
-            pos2.y += speed;
+            pos1 = player.transform.position;
+            pos2 = player.transform.position;
+            linePoints[0] = pos1;
+            linePoints[1] = pos2;
+            SetPositions(linePoints);
         }
 
-        //ワールド座標をリストに追加
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isHit == true)
+            {
+                isExtend = false;
+                isHit = false;
+                pos2 = player.transform.position;
+                linePoints[0] = pos1;
+                linePoints[1] = pos2;
+                SetPositions(linePoints);
+
+
+            }
+            else
+            {
+                isExtend = true;
+            }
+        }
+
+        if (isHit == false && isExtend == true)
+        {
+            pos2.y += speed;
+            //当たり判定の設定
+            linePoints[1] = pos2;
+            SetPositions(linePoints);
+        }
+
+
+        /*
         linePoints.Add(pos2);
-        //EdgeCollider2Dのポイントを設定
         line.GetComponent<EdgeCollider2D>().SetPoints(linePoints);
 
-
-
+        //頂点を追加
         positions = new Vector3[]
         { pos1,pos2 };
 
+        //頂点の数
         line.positionCount = positions.Length;
 
+        //線を書く
         line.SetPositions(positions);
+        */
+    }
+
+    void SetPositions(List<Vector3> positions)
+    {
+        List<Vector2> positions2D = new List<Vector2>();
+        for (int i = 0; i < positions.Count; i++)
+        {
+            positions2D.Add(new Vector2(positions[i].x, positions[i].y));
+        }
+        line.GetComponent<EdgeCollider2D>().SetPoints(positions2D);
+        line.positionCount = positions.Count;
+        line.SetPositions(positions.ToArray());
+    }
+
+    private void ResetPoint(List<Vector3> linePoints)
+    {
+        linePoints[0] = pos1;
+        linePoints[1] = pos1;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -67,6 +125,8 @@ public class Line : MonoBehaviour
         if (collision.tag == "Box" || collision.tag == "Block")
         {
             isHit = true;
+            box = collision.gameObject;
+            pos2.y = box.transform.position.y;
         }
     }
 }
